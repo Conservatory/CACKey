38215c795503bec53121a586f691a357f28acbdc
rkeene <rkeene>
2012-07-30 05:07:01 +0000

    Moved copying in of automake/autoconf files to autogen from build

---
+++ autogen.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+rm -f aclocal.m4
+
+${MAKE:-make} -C aclocal
+autoconf; autoheader
+
+rm -rf autom4te.cache/
+
+for basefile in install-sh config.sub config.guess; do
+	for path in /usr/share/automake-*; do
+		file="${path}/${basefile}"
+		if [ -f "${file}" ]; then
+			cp "${file}" .
+			chmod 755 "./${basefile}"
+
+			break
+		fi
+	done
+done
--- build/build.sh
+++ build/build.sh
@@ -2,6 +2,7 @@
 
 find . -type f -name '.*.sw?' | xargs rm -f
 find . -type f -name '.nfs*' | xargs rm -f
+find . -type f -name '*~' | xargs rm -f
 
 if [ "${SNAPSHOT}" = "1" ]; then
 	sed "s@\(AC_INIT([^)]*\))@\1.${VERS})@" configure.ac > configure.ac.new
@@ -14,18 +15,6 @@ fi
 if [ ! -x configure ]; then
 	exit 1
 fi
-
-for basefile in install-sh config.sub config.guess; do
-	for path in /usr/share/automake-*; do
-		file="${path}/${basefile}"
-		if [ -f "${file}" ]; then
-			cp "${file}" .
-			chmod 755 "./${basefile}"
-
-			break
-		fi
-	done
-done
 
 if [ "${SNAPSHOT}" != "1" ]; then
 	mv build build_delete
