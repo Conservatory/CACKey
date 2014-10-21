69edcf2d859f3ade4a4ca2181e946b25ced7279c
kvanals <kvanals>
2014-10-21 23:01:27 +0000

    Updated email addresses, cleaned up Mac OS X build script, and modified the Mac OS X build script to work on a build host running Mac OS X 10.10 (Yosemite).

--- build/cackey_osx_build/build_osx.sh
+++ build/cackey_osx_build/build_osx.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Shell Script to make Mac OS X Releases of CACKey
 # Kenneth Van Alstyne
-# DC1SAN_SUPPORT@hq.dhs.gov
+# kenny@coreadaptive.com
 CACKEY_VERSION=`cat configure.ac | grep AC_INIT | cut -d " " -f 2 | sed 's_)__'`
 
-# Check to see if we're building on Mac OS X 10.7 "Lion"
-if [ "`uname -r | cut -d . -f 1`" = "11" ]; then
+# Check to see if we're building on Mac OS X 10.7 "Lion" or newer
+if [ "`uname -r | cut -d . -f 1`" -ge "11" ]; then
 	LIONBUILD=1
 fi
 
@@ -14,12 +14,16 @@ usage() {
 	echo "Usage: build_osx.sh <target>"
 	echo Where target is one of:
 	echo "    leopard  - (Builds Universal 10.5 Library for PPCG4/i386)"
-	echo "    snowleopard  - (Builds Universal 10.6 Library for i386/x86_64)"
-	echo "    lion  - (Builds Universal 10.7 Library for i386/x86_64)"
 	echo "    sltoyos - (Builds Universal 10.6/10.7/10.8/10.9/10.10 Library for i386/x86_64)"
 	echo "    all - (Builds for all supported targets)"
 	echo "    clean - (Cleans up)"
 	echo "Run from CACKey Build Root."
+	echo ""
+	echo "NOTE:  Leopard build requires legacy XCode 3 components in"
+	echo "       /Developer because of PowerPC support."
+	echo "       All builds require gnutar, automake, and autoconf."
+	echo "       If your newer release of XCode does not include these"
+	echo "       components, I recommend installing them from MacPorts."
 	exit $?
 }
 
@@ -43,8 +47,6 @@ makedir() {
 	if [ ! -d macbuild ]; then
 		mkdir macbuild
 		mkdir macbuild/Leopard
-		mkdir macbuild/Snowleopard
-		mkdir macbuild/Lion
 		mkdir macbuild/Sltoyos
 		mkdir macbuild/pkg
 	fi
@@ -78,64 +80,7 @@ leopard() {
 	pkgbuild
 }
 
-# Build function for Snow Leopard
-snowleopard() {
-	makedir
-	HEADERS=/Developer/SDKs/MacOSX10.6.sdk/System/Library/Frameworks/PCSC.framework/Versions/A/Headers/
-	LIBRARY=/Developer/SDKs/MacOSX10.6.sdk/System/Library/Frameworks/PCSC.framework/PCSC
-	LIB=""
-	ARCHLIST=""
-	DLIB=""
-	DARCHLIST=""
-	OSX=Snowleopard
-	PKTARGETOS=3
-	CUROSXVER=10.6
-	for HOST in i386-apple-darwin10 x86_64-apple-darwin10; do
-		genbuild
-	done
-	libbuild
-	pkgbuild
-}
-
-# Build function for Lion
-lion() {
-	makedir
-	HEADERS=/Developer/SDKs/MacOSX10.7.sdk/System/Library/Frameworks/PCSC.framework/Versions/A/Headers/
-	LIBRARY=/Developer/SDKs/MacOSX10.7.sdk/System/Library/Frameworks/PCSC.framework/PCSC
-	LIB=""
-	ARCHLIST=""
-	DLIB=""
-	DARCHLIST=""
-	OSX=Lion
-	PKTARGETOS=3
-	CUROSXVER=10.7
-	for HOST in i386-apple-darwin11 x86_64-apple-darwin11; do
-		genbuild
-	done
-	libbuild
-	pkgbuild
-}
-
-# Build function for Snow Leopard/Lion/Mountain Lion
-sltoyos() {
-	makedir
-	HEADERS=/Developer/SDKs/MacOSX10.6.sdk/System/Library/Frameworks/PCSC.framework/Versions/A/Headers/
-	LIBRARY=/Developer/SDKs/MacOSX10.6.sdk/System/Library/Frameworks/PCSC.framework/PCSC
-	LIB=""
-	ARCHLIST=""
-	DLIB=""
-	DARCHLIST=""
-	OSX=Sltoyos
-	PKTARGETOS=3
-	CUROSXVER=10.6
-	for HOST in i386-apple-darwin10 x86_64-apple-darwin10; do
-		genbuild
-	done
-	libbuild
-	pkgbuild
-}
-
-# Build function for Snow Leopard/Lion/Mountain Lion
+# Build function for Snow Leopard/Lion/Mountain Lion/Mavericks/Yosemite
 sltoyos() {
 	makedir
 	HEADERS=/Developer/SDKs/MacOSX10.6.sdk/System/Library/Frameworks/PCSC.framework/Versions/A/Headers/
@@ -159,15 +104,11 @@ genbuild() {
 	make distclean
 	ARCH=`echo ${HOST} | cut -d "-" -f 1`
 	if [ ${ARCH} == "powerpc" ]; then
-		if [ ${OSX} == "Leopard" ]; then
-			ARCH="ppc -mcpu=G4"
-		else
-			ARCH="ppc -mcpu=G3"
-		fi
+		ARCH="ppc -mcpu=G4"
 	fi
 	if [ "${LIONBUILD}" = 1 ]; then
 		if [ "${ARCH}" == "ppc -mcpu=G4" ]; then
-			CC=powerpc-apple-darwin11-gcc-4.2.1 CPP=powerpc-apple-darwin11-cpp-4.2.1 CFLAGS="-m32 -I/Developer/Platforms/iPhoneOS.platform/Developer/usr/lib/gcc/powerpc-apple-darwin10/4.2.1/include -isysroot /Developer/SDKs/MacOSX10.5.sdk" CPPFLAGS="-D_LIBC_LIMITS_H_" ./configure --with-pcsc-headers=${HEADERS} --with-pcsc-libs=${LIBRARY} --host=${HOST} --enable-dod-certs-on-hw-slots
+			CC=/Developer/usr/bin/powerpc-apple-darwin10-gcc-4.2.1 CXX=/Developer/usr/bin/powerpc-apple-darwin10-g++-4.2.1 CFLAGS="-m32 -mcpu=G4 -I/Developer/usr/lib/gcc/powerpc-apple-darwin10/4.2.1/include -isysroot /Developer/SDKs/MacOSX10.5.sdk" CPPFLAGS="-D_LIBC_LIMITS_H_" ./configure --with-pcsc-headers=${HEADERS} --with-pcsc-libs=${LIBRARY} --host=${HOST} --enable-dod-certs-on-hw-slots
 		else
 			CFLAGS="-arch ${ARCH}" ./configure --with-pcsc-headers=${HEADERS} --with-pcsc-libs=${LIBRARY} --host=${HOST} --enable-dod-certs-on-hw-slots
 		fi
@@ -208,10 +149,10 @@ pkgbuild() {
 		TAR=tar
 	fi
 	if [ "${OSX}" = "Leopard" ]; then
-		curl http://devel.kvanals.org/PKCS11_Tokend/PKCS11_tokend-latest.tar.gz | ${TAR} --strip-components 3 --wildcards -x -z -f - "PKCS11_tokend-*/prebuilt/leopard/PKCS11.tokend"
+		curl http://devel.kvanals.org/PKCS11_Tokend/PKCS11_tokend-latest.tar.gz | gzip -d -c | ${TAR} --strip-components 3 --wildcards -x -f - "PKCS11_tokend-*/prebuilt/leopard/PKCS11.tokend"
 		mv PKCS11.tokend build/cackey_osx_build/PKCS11.tokend
 	else
-		curl http://devel.kvanals.org/PKCS11_Tokend/PKCS11_tokend-latest.tar.gz | ${TAR} --strip-components 3 --wildcards -x -z -f - "PKCS11_tokend-*/prebuilt/snowleopard/PKCS11.tokend"
+		curl http://devel.kvanals.org/PKCS11_Tokend/PKCS11_tokend-latest.tar.gz | gzip -d -c | ${TAR} --strip-components 3 --wildcards -x -f - "PKCS11_tokend-*/prebuilt/snowleopard/PKCS11.tokend"
 		mv PKCS11.tokend build/cackey_osx_build/PKCS11.tokend
 	fi
 	for PMDOC in build/cackey_osx_build/Template_pmbuild/*.in; do
@@ -228,10 +169,6 @@ pkgbuild() {
 		mv build/cackey_osx_build/${OSX}_pmbuild.pmdoc/${PMDOC}.1 build/cackey_osx_build/${OSX}_pmbuild.pmdoc/${PMDOC}
 	done
 	EXT=pkg
-	if [ ${OSX} == "Snowleopard" ]; then
-		cat build/cackey_osx_build/${OSX}_pmbuild.pmdoc/index.xml | sed 's|for Mac OS X Snowleopard|for Mac OS X SnowLeopard|g' > build/cackey_osx_build/${OSX}_pmbuild.pmdoc/index.xml.new
-		mv build/cackey_osx_build/${OSX}_pmbuild.pmdoc/index.xml.new build/cackey_osx_build/${OSX}_pmbuild.pmdoc/index.xml
-	fi
 	if [ ${OSX} == "Sltoyos" ]; then
 		cat build/cackey_osx_build/${OSX}_pmbuild.pmdoc/index.xml | sed 's|for Mac OS X Sltoyos|for Mac OS X SLtoYos|g' > build/cackey_osx_build/${OSX}_pmbuild.pmdoc/index.xml.new
 		mv build/cackey_osx_build/${OSX}_pmbuild.pmdoc/index.xml.new build/cackey_osx_build/${OSX}_pmbuild.pmdoc/index.xml
@@ -258,18 +195,6 @@ case "$1" in
 		exit $?
 	;;
 
-	"snowleopard")
-		./autogen.sh
-		snowleopard
-		exit $?
-	;;
-	
-	"lion")
-		./autogen.sh
-		lion
-		exit $?
-	;;
-	
 	"sltoyos")
 		./autogen.sh
 		sltoyos
