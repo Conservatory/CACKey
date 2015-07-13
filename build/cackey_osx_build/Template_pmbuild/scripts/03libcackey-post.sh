dc246a89ff564f01566e77b60eaf4249ee8503d0
kvanals <kvanals>
2015-07-13 20:21:50 +0000

    Updated Mac OS X Build to support installation on 10.11 "El Capitan" after external project (TokenD) did the same

---
+++ build/cackey_osx_build/Template_pmbuild/scripts/03libcackey-post.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+chmod 755 /usr/local/lib/pkcs11
+chown root:wheel /usr/local/lib/pkcs11
---
+++ build/cackey_osx_build/Template_pmbuild/scripts/04pkcs11tokend-post.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+chmod -R go+rX /Library/Security/tokend/PKCS11.tokend
+chown -R root:wheel /Library/Security/tokend/PKCS11.tokend
